7610aa20cc2b467eab540a2d239d6476393b6e1d
Mitja V. Iskric <mitja.iskric@gmail.com>
2015-10-16 13:20:32 +0200

    create main.py
    prvo

---
+++ main.py
@@ -0,0 +1,12 @@
+ime1 = "Andreja"
+ime2 = "Maša"
+delovno_mesto1 = "izposoja 1"
+delovno_mesto2 = "interno delo"
+
+print ("Urnik za danes: ")
+print (ime1, "=>", delovno_mesto1)
+print (ime2, "=>", delovno_mesto2)
+print
+print ("Urnik za jutri: ")
+print (ime1, "=>", delovno_mesto1)
+print (ime2, "=>", delovno_mesto2)
