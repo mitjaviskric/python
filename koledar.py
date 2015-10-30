426f181310357f92d0b617f1bbc0955a85dde1f2
Mitja V. Iskric <mitja.iskric@gmail.com>
2015-10-30 16:43:06 +0100

    Update koledar.py

--- koledar.py
+++ koledar.py
@@ -1,10 +1,11 @@
+## OPOZORILO: deluje le, če je stevilo imen manjše ali enako stevilu delovnih mest!
 i = 0     ## stevec imen
 d = 0     ## stevec delovnih mest
 ij = 0    ## nov stevec imen za jutri
-dj = -1   ## stevec delovnih mest za jutri z novim razporedom, premaknjenim za eno
+dj = -4   ## stevec delovnih mest za jutri z novim razporedom, premaknjenim za eno
 
-ime = ["Andreja", "Masa", "Sara"]
-delovno_mesto = ["izposoja 1", "izposoja 2", "interno delo"]
+ime = ["Andreja V. I.", "Masa", "Sara", "Dusanka"]
+delovno_mesto = ["izposoja 1", "izposoja 2", "izposoja 3", "interno delo", "interno delo 2"]
 
 print ()
 print ("Urnik za danes: ")
@@ -18,7 +19,7 @@ print ()
 print ("Urnik za jutri: ")
 
 for vsakoime in ime:
-    print (ime[ij], "=>",delovno_mesto [dj])
+    print (ime[ij], "=>",delovno_mesto [dj]) ## i in d so ze porabljeni
     ij += 1
     dj += 1
 
