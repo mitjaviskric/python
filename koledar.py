3558491d6caf527b35fd1e0584358c8cd868dc99
Mitja V. Iskric <mitja.iskric@gmail.com>
2015-10-30 17:53:12 +0100

    Update koledar.py
    ostala imena, ki ne pridejo na seznam delovnih mest gredo na interno

--- koledar.py
+++ koledar.py
@@ -1,10 +1,10 @@
-## OPOZORILO: deluje le, če je število delovnih mest manjše ali enako številu imen!
-x = 0     ## števec 
-xj = 0    ## nov števec za jutri
+x = 0     # števec 
+xj = 0    # nov števec za jutri
 
 
-delovna_mesta = ["izposoja 1", "izposoja 2", "izposoja 3", "interno delo"]
-imena = ["Andreja V. I.", "Masa", "Sara", "Karmen", "Andreja"]
+izposojevalna_mesta = ["izposoja 1", "izposoja 2", "izposoja 3", "izposoja 4"]
+ostala_delovna_mesta = ["interno delo 1", "interno delo 2"]
+imena = ["Andreja V. I.", "Masa", "Sara", "Karmen", "Andreja", "Andreja P.", "Mateja"]
 imena_jutri = imena[:]       #kopiramo seznam
 del imena_jutri[0]           #izbrišemo prvo mesto
 imena_jutri.append(imena[0]) #dodamo na koncu prvo vrednost iz prejšnjega seznama
@@ -12,15 +12,21 @@ imena_jutri.append(imena[0]) #dodamo na koncu prvo vrednost iz prejšnjega sezna
 print ()
 print ("Urnik za danes: ")
 
-for delovno_mesto in delovna_mesta:
+for delovno_mesto in izposojevalna_mesta:
     print (delovno_mesto, "=>",imena [x])
     x += 1
     
 print ()
+print ("Interno delo:")
+print (imena [x:(len(imena))]) #izpiše imena, ki so ostala
+print ()
 print ("Urnik za jutri: ")
 
-for delovno_mesto in delovna_mesta:
-    print (delovno_mesto, "=>",imena_jutri [xj]) ## d so že porabljeni
+for delovno_mesto in izposojevalna_mesta:
+    print (delovno_mesto, "=>",imena_jutri [xj]) # d so že porabljeni
     xj += 1
     
+print ()
+print ("Interno delo jutri:")
+print (imena_jutri [xj:(len(imena_jutri))])
 print ()
