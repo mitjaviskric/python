8f4b9b11cdbb03b28111ce5f2174f2bce116ab59
Mitja V. Iskric <mitja.iskric@gmail.com>
2015-10-30 17:04:16 +0100

    Update koledar.py
    kopiranje seznama

--- koledar.py
+++ koledar.py
@@ -2,10 +2,13 @@
 i = 0     ## števec imen
 d = 0     ## števec delovnih mest
 ij = 0    ## nov števec imen za jutri
-dj = -4   ## števec delovnih mest za jutri z novim razporedom, premaknjenim za eno
+dj = 0   ## števec delovnih mest za jutri z novim razporedom, premaknjenim za eno
 
 ime = ["Andreja V. I.", "Masa", "Sara", "Dusanka"]
 delovno_mesto = ["izposoja 1", "izposoja 2", "izposoja 3", "interno delo", "interno delo 2"]
+delovno_mesto_jutri = delovno_mesto[:]       #kopiramo seznam
+del delovno_mesto_jutri[0]                   #izbrišemo prvo mesto
+delovno_mesto_jutri.append(delovno_mesto[0]) #dodamo na koncu prvo vrednost iz prejšnjega seznama
 
 print ()
 print ("Urnik za danes: ")
@@ -19,8 +22,8 @@ print ()
 print ("Urnik za jutri: ")
 
 for vsakoime in ime:
-    print (ime[ij], "=>",delovno_mesto [dj]) ## i in d so že porabljeni
+    print (ime[ij], "=>",delovno_mesto_jutri [dj]) ## i in d so že porabljeni
     ij += 1
     dj += 1
-
+    
 print ()
