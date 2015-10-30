dcd67653ed9f643c9592945acd8083d4aa0d9659
Mitja V. Iskric <mitja.iskric@gmail.com>
2015-10-30 17:12:19 +0100

    Update koledar.py
    ime je že spremenljivka določena v for zanki

--- koledar.py
+++ koledar.py
@@ -1,29 +1,25 @@
 ## OPOZORILO: deluje le, če je število imen manjše ali enako številu delovnih mest!
-i = 0     ## števec imen
 d = 0     ## števec delovnih mest
-ij = 0    ## nov števec imen za jutri
 dj = 0    ## nov števec delovnih mest za jutri
 
-ime = ["Andreja V. I.", "Masa", "Sara", "Dusanka"]
-delovno_mesto = ["izposoja 1", "izposoja 2", "izposoja 3", "interno delo", "interno delo 2"]
-delovno_mesto_jutri = delovno_mesto[:]       #kopiramo seznam
-del delovno_mesto_jutri[0]                   #izbrišemo prvo mesto
-delovno_mesto_jutri.append(delovno_mesto[0]) #dodamo na koncu prvo vrednost iz prejšnjega seznama
+imena = ["Andreja V. I.", "Masa", "Sara", "Dusanka"]
+delovna_mesta = ["izposoja 1", "izposoja 2", "izposoja 3", "interno delo", "interno delo 2"]
+delovna_mesta_jutri = delovna_mesta[:]       #kopiramo seznam
+del delovna_mesta_jutri[0]                   #izbrišemo prvo mesto
+delovna_mesta_jutri.append(delovna_mesta[0]) #dodamo na koncu prvo vrednost iz prejšnjega seznama
 
 print ()
 print ("Urnik za danes: ")
 
-for vsakoime in ime:
-    print (ime[i], "=>",delovno_mesto [d])
-    i += 1
+for ime in imena:
+    print (ime, "=>",delovna_mesta [d])
     d += 1
     
 print ()
 print ("Urnik za jutri: ")
 
-for vsakoime in ime:
-    print (ime[ij], "=>",delovno_mesto_jutri [dj]) ## i in d so že porabljeni
-    ij += 1
+for ime in imena:
+    print (ime, "=>",delovna_mesta_jutri [dj]) ## d so že porabljeni
     dj += 1
     
 print ()
