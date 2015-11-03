bbd68220e31be2b6b95fc4ad6f0e7a33bd3a4bf3
Mitja <mitja.iskric@gmail.com>
2015-11-03 13:00:15 +0000

    initial commit

--- koledar.py
+++ koledar.py
@@ -1,15 +1,17 @@
-x = 0     # števec 
+x = 0     # števec
 y = 0     # nov števec za jutri
 
 izposojevalna_mesta = ["izposoja 1", "izposoja 2", "izposoja 3", "izposoja 4", "informacije 1"]
-imena = ["Andreja V. I.", "Masa", "Sara", "Karmen", "Andreja", "Andreja P.", "Mateja"]
-manjka = input ("Vnesite kdo manjka:")
-imena.remove(manjka)         #izbrišemo manjkajoče iz seznama imena
+imena = ["Andreja VI", "Maša", "Sara", "Karmen", "Andreja", "Andreja P", "Mateja"]
 imena_jutri = imena[:]       #kopiramo seznam
-del imena_jutri[0]           #izbrišemo prvo mesto
+del imena_jutri[0]           #izbrišemo prvo ime
 imena_jutri.append(imena[0]) #dodamo na koncu prvo vrednost iz prejšnjega seznama
-manjka_jutri = input ("Kdo bo manjkal jutri:")
-imena_jutri.remove(manjka_jutri)
+manjka = input ("Vnesite kdo manjka:")
+if manjka != "":             # če [enter] pojdi naprej
+    imena.remove(manjka)     #izbrišemo ime, če manjka
+manjka_jutri = input ("Vnesite, kdo bo manjkal jutri:")
+if manjka_jutri != "":
+    imena_jutri.remove(manjka_jutri)
 
 print ()
 print ("Urnik za danes: ")
@@ -17,7 +19,7 @@ print ("Urnik za danes: ")
 for delovno_mesto in izposojevalna_mesta:
     print (delovno_mesto, "=>",imena [x])
     x += 1
-    
+
 print ()
 print ("Interno delo:")
 print (imena [x:(len(imena))]) #izpiše imena, ki so ostala
@@ -27,7 +29,7 @@ print ("Urnik za jutri: ")
 for delovno_mesto in izposojevalna_mesta:
     print (delovno_mesto, "=>",imena_jutri [y]) # x so že porabljeni
     y += 1
-    
+
 print ()
 print ("Interno delo jutri:")
 print (imena_jutri [y:(len(imena_jutri))])
