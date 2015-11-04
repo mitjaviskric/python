702e8e94bb86d921c804bab83bda3734a487efb1
Mitja <mitja.iskric@gmail.com>
2015-11-04 11:10:03 +0000

    popravil drug dan na konec

--- koledar.py
+++ koledar.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # prikaz koledarja za dva dni
 # todo če nekdo manjka, potem je lahko tisti, ki nadomešča, drug dan na istem mestu...
 
@@ -6,15 +8,23 @@ y = 0     # nov števec za jutri
 
 izposojevalna_mesta = ["izposoja 1", "izposoja 2", "izposoja 3", "izposoja 4", "informacije 1"]
 imena = ["Andreja VI", "Maša", "Sara", "Karmen", "Andreja", "Andreja P", "Mateja"]
-imena_jutri = imena[:]       #kopiramo seznam
-del imena_jutri[0]           #izbrišemo prvo ime
-imena_jutri.append(imena[0]) #dodamo na koncu prvo vrednost iz prejšnjega seznama
+
 manjka = input ("Vnesite kdo manjka:")
 if manjka != "":             # če [enter] pojdi naprej
     imena.remove(manjka)     #izbrišemo ime, če manjka
+
+# jutri
+
+imena_jutri = imena[:]       #kopiramo seznam
+del imena_jutri[0]           #izbrišemo prvo ime
+imena_jutri.append(imena[0]) #dodamo na koncu prvo vrednost iz prejšnjega seznama
+
 manjka_jutri = input ("Vnesite, kdo bo manjkal jutri:")
-if manjka_jutri != "":
-    imena_jutri.remove(manjka_jutri)
+if manjka_jutri != "":       # če [enter] pojdi naprej
+    imena_jutri.remove(manjka_jutri) #izbrišemo ime, če manjka jutri
+
+imena_jutri.remove(manjka)   #tisti, ki manjka danes, a ne bo jutri, gre na konec
+imena_jutri.append(manjka)   #na konec
 
 print ()
 print ("Urnik za danes: ")
