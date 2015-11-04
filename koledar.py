d2f5cbdaf793f39962d6bc8e8e81b61046a4548f
Mitja <mitja.iskric@gmail.com>
2015-11-04 14:05:31 +0000

    opombe

--- koledar.py
+++ koledar.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 # prikaz koledarja za dva dni
-# To do: vnos več ljudi naenkrat.
+# todo: premik ostalih za dva!
+# todo: vnos več ljudi naenkrat.
 
 x = 0     # števec
 y = 0     # nov števec za jutri
 
 izposojevalna_mesta = ["izposoja 1", "izposoja 2", "izposoja 3", "izposoja 4", "informacije 1"]
 imena = ["Andreja VI", "Maša", "Sara", "Karmen", "Andreja", "Andreja P", "Mateja"]
-
 # seznam za jutri
 imena_jutri = imena[:]       # kopiramo seznam
 del imena_jutri[0]           # izbrišemo prvo ime
@@ -17,8 +17,8 @@ imena_jutri.append(imena[0]) # dodamo na koncu prvo vrednost iz prejšnjega sezn
 manjka = input ("Vnesite kdo manjka:")
 if manjka != "":             # če [enter] pojdi naprej
     imena.remove(manjka)     # izbrišemo ime, če manjka
-    imena_jutri.remove(manjka)   # tisti, ki manjka danes, a ne bo jutri, gre na konec
-    imena_jutri.append(manjka)   # na konec
+    imena_jutri.remove(manjka)   # tisti, ki manjka danes, a ne bo jutri, gre
+    imena_jutri.append(manjka)   # na konec (da se ostali premaknejo) - NAPAKA? - premaknejo se za dva!
 
 # jutri
 manjka_jutri = input ("Vnesite, kdo bo manjkal jutri:")
